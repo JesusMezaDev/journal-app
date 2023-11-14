7118980337888019605fb409463db0e439b0b16c
Jesús Meza <jame181185@gmail.com>
2023-11-14 08:23:49 -0700

    Se hacen pequeñas correcciones de estilos en el listado de las entradas.

--- src/modules/daybook/components/EntryList.vue
+++ src/modules/daybook/components/EntryList.vue
@@ -3,7 +3,7 @@
         <div class="px-2 pt-2">
             <input type="text" class="form-control" placeholder="Buscar entrada" v-model="searchTerm" @change="getEntriesByTerm">
         </div>
-        <div class="mt-2 d-flex flex-column">
+        <div class="my-2 d-flex flex-column">
             <button class="btn btn-primary mx-3 bi bi-plus-circle" @click="$router.push({ name: 'entry', params: { id: 'new' } })">
                 Nueva entrada
             </button>
@@ -28,11 +28,11 @@
 <style lang="scss" scoped>
     .entry-list-container {
         border-right: 1px solid #2C3E50;
-        height: calc(100vh - 56px);
+        height: calc(100vh - 60px);
     }
 
     .entry-scrollarea {
-        height: calc(100vh - 110px);
-        overflow: scroll;
+        height: calc(100vh - 18%);
+        overflow-y: scroll;
     }
 </style>
