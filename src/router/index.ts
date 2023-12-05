be6f17624954a8e99d578a67087b33b9de742010
Jesús Meza <jame181185@gmail.com>
2023-12-05 13:26:48 -0700

    Se cambia por hashHistory en el router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router';
 
 import HomeView from '@/views/HomeView.vue';
 
@@ -6,7 +6,7 @@ import DaybookRouter from '@/modules/daybook/router';
 import AuthRouter from '@/modules/auth/router';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL), //createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
